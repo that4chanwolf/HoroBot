1f051a2357077b4737daf5fc99fa5a2ded000e2e
that4chanwolf <that4chanwolf@gmail.com>
2012-07-13 18:08:52 -0500

    made config file look nicer, added in the lolmodules section

--- config.js
+++ config.js
@@ -1,14 +1,16 @@
 exports.Config = {
-    'network': 'irc.rizon.net',
-    'userName': 'HoroBot',
-    'nickName': 'HoroBot',
-    'realName': 'Thread *Chan Bot',
-    'channel': '#chan',
-    'NSPassword': 'nickServPassword',
-    'topicFile': 'topic.txt',
-    'allowedUsers': [
+    network: 'irc.rizon.net',
+    userName: 'HoroBot',
+    nickName: 'HoroBot',
+    realName: 'Thread *Chan Bot',
+    channel: '#chan',
+    NSPassword: 'nickServPassword',
+    topicFile: 'topic.txt',
+    allowedUsers: [
         'user1',
         'user2',
         'user3'
+    ],
+    modules: [
     ]
 };
