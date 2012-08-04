b4ba8bd6a3a5617f3c25f77b2152af046fb69cff
Wise Horo <that4chanwolf@gmail.com>
2012-08-04 08:52:05 -0400

    change config and modules file to actually work

--- modules.js
+++ modules.js
@@ -1,14 +1,2 @@
-/*
- * MODULES LAYOUT:
- * exports.modules = {
- *     moduleName: [
- *         /^\$regex/,
- *         // NOTE: You are required to pass in the `client` and `rc` arguments as part of your module
- *         function(args) { 
- *             client.command(arg1, arg2);
- *         }
- *     ]
- * }
- * Also, Suiseiseki is a trap.
- */
-
+{
+}
