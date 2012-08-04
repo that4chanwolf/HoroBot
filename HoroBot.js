a9b7c74ed3eaa707abcd4b95d3a705efc5060845
Wise Horo <that4chanwolf@gmail.com>
2012-08-04 09:29:02 -0400

    reintroduce the log, set/writeTopic, and threadCheck functions

--- HoroBot.js
+++ HoroBot.js
@@ -13,3 +13,43 @@ var topic, title, thread, extra;
  */
 var rc = JSON.parse(fs.readFileSync('config.js', 'utf8'));
 var modules = JSON.parse(fs.readFileSync('modules.js', 'utf8'));
+
+/*
+ * Our logging function.
+ * Spits out the date and the message
+ */
+var log = function(msg) {
+	console.log(new Date() + ' - [!] - ' + msg);
+};
+
+/*
+ * Our setTopic and writeTopic functions. 
+ * Pretty self-explanitory.
+ */
+var setTopic = function(data) {
+	title = data['title'];
+	thread = data['thread'];
+	extra = data['extra'];
+	topic = title + irc.colors.yellow + ' || ' + irc.colors.reset + 'Current Thread: ' + thread + irc.colors.yellow + ' || ' + irc.colors.reset + extra;
+};
+var writeTopic = function(data) {
+	fs.writeFile(rc.topicFile, JSON.stringify(data), function() {
+		log('Topic written to file');
+	});
+};
+
+/*
+ * Our thread checker function.
+ * It alerts the the chat if a thread 404s, and removes it from the title.
+ * Yes, I know global variables are bad, blah blah blah, shut up.
+ */
+var threadCheck = function(thread) {
+	if(GLOBAL.tcheck !== undefined && GLOBAL.tcheck !== null) {
+		clearInterval(GLOBAL.tcheck);
+	}
+	var ts = thread.split('/');
+	var tpath = '/' + ts[3] + '/' + ts[4] + '/' + ts[5];
+	GLOBAL.tcheck = setInterval(function(tpath) {
+		
+	}, 30000);
+};
