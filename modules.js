65f657fe930b24b06c36ad9778f68ebe948dafe9
Wise Horo <that4chanwolf@gmail.com>
2012-11-16 07:28:48 -0500

    Better examples

--- modules.js
+++ modules.js
@@ -15,10 +15,21 @@
 exports.modules = {
 	//Example: Pet a person at random
 	pets: [
-		//,
+		/(?:)/,
 		function(client, rc, nick) {
 			if( Math.floor( Math.random()*101 ) < 12 ) {
 				client.action(rc.channel, 'pets ' + nick);
+			}
+		}
+	],
+	//Example: Kick people who say lel or le
+	lel: [
+		/lel/i,
+		function(client, rc, nick, message) {
+			var s = message.split(" ");
+			if(s.indexOf("lel") !== -1 || s.indexOf("le") !== -1) {
+				console.error("Reddit detected, removing");
+				client.conn.write('KICK ' + rc.channel + ' ' + nick + ' :Stop shitposting!\r\n');
 			}
 		}
 	]
